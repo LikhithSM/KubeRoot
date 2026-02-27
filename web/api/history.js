39e1609c50296b04a067889cc6bc9a80d965ced9
Likhith S M <smlikhith25@gmail.com>
2026-02-27 23:31:15 +0530

    Proxy attaches server API key

--- web/api/history.js
+++ web/api/history.js
@@ -7,7 +7,11 @@ export default async function handler(req, res) {
       'Content-Type': 'application/json',
     };
 
-    // forward client X-API-Key if present
+    // include server-side API key so clients don't have to know it
+    if (process.env.VITE_API_KEY) {
+      headers['X-API-Key'] = process.env.VITE_API_KEY;
+    }
+    // also forward any client header just in case (it will be overwritten above)
     if (req.headers['x-api-key']) {
       headers['X-API-Key'] = req.headers['x-api-key'];
     }
